b7d7086dac6b074d25382d370b023364cba6737e
Blair Anderson <blair81@gmail.com>
2020-12-17 16:36:47 -0800

    update the header

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <p className="h5 my-0 me-md-auto fw-normal">Emailazon</p>
+      <p className="h5 my-0 me-md-auto fw-normal">EmailAZON / AmazonCSV</p>
       <nav className="my-2 my-md-0 me-md-3">
         <a className="p-2 text-dark" href="/">
           Features
